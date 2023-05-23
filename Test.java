c8a56414794d2fa01284412e0113796bb0c7480d
SAAM SERViCE <haniflakeu@gmail.com>
2023-05-23 18:34:27 +0200

    added more italian because italians dont usually know how to speak english

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static  void main(String[] args){
         System.out.println("Hello from my projet");
+        System.out.println("BuonaSera agli italiani in ascolto");
     }
 }
