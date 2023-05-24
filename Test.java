3ff02ad563da315fad2924b5dcd15173522f119c
SAAM SERViCE <haniflakeu@gmail.com>
2023-05-24 15:36:46 +0200

    added outline

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static  void main(String[] args){
         System.out.println("Hello from my projet");
         System.out.println("BuonaSera agli italiani in ascolto");
-
+        System.out.println("Another new line of -------");
     }
 }
